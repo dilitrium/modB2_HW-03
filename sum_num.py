c2af49e0261f5b594e509fe93c04b3db3ec25d48
dilitrium <dilitrium@gmail.com>
2023-11-11 18:40:14 +0300

    Update - добавил в функцию 4 число

--- sum_num.py
+++ sum_num.py
@@ -1,4 +1,5 @@
 number1=int(input("Введите первое число:"))
 number2=int(input("Введите второе число:"))
 number3=int(input("Введите третье число:"))
-print("Сумма чисел равна", number1+number2+number3)
+number4=int(input("Введите четвертое число:"))
+print("Сумма чисел равна", number1+number2+number3+number4)
